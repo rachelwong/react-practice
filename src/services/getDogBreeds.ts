6a81c9a2808bc5c933b33db2991e2dd5dc0cecdf
rachelwong <hello.rachelwong@gmail.com>
2026-09-16 08:11:33 +1000

    fix: use fetch isntad in getDogbreeds

--- src/services/getDogBreeds.ts
+++ src/services/getDogBreeds.ts
@@ -1,11 +1,14 @@
-import axios from "axios";
 import type { DogBreedListResponse } from "./types/DogBreedResponse";
 
 const getDogBreeds = async (): Promise<DogBreedListResponse | null> => {
   const baseUrl = `https://dog.ceo/api/breeds/list/all`;
   try {
-    const { data } = await axios.get(baseUrl);
-    return data;
+    const res = await fetch(baseUrl);
+    // avoid using .then/.catch so that the try/catch throws the error instead
+    if (res.status === 200) {
+      return await res.json();
+    }
+    return null;
   } catch (err) {
     console.error(`GetDogBreeds service error, ${err}`);
     throw err;
