bf681650741f924d9d427ff26d26fd23e2f1d65c
rachelwong <hello.rachelwong@gmail.com>
2026-09-19 15:17:01 +1000

    chore: update expense route config

--- src/constants/Routes.ts
+++ src/constants/Routes.ts
@@ -122,7 +122,7 @@ export const ROUTE_CONFIG: RouteConfig[] = [
   {
     route: ROUTES.EXPENSE_TRACKER,
     title: "Expense tracker x At-home coffee investment break-even Calculator",
-    description: "Dynamic table with redux & useReducer",
-    inProgress: true,
+    description:
+      "Dynamic table with redux, useReducer, localStorage persistence",
   },
 ];
